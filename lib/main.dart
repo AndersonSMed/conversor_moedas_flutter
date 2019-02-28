b262c6227798346ac1abc9ca116f666193857805
Anderson Santos <anderson.santos.medeiros@hotmail.com>
2019-02-28 09:46:29 -0300

    Added http request to get info from the API

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Home());
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+const request = 'https://api.hgbrasil.com/finance?format=json&key=f228edf4';
+
+void main() async {
+  runApp(MaterialApp(home: Home()));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
 }
 
 class Home extends StatefulWidget {
@@ -12,6 +23,41 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text("\$ Conversor \$"),
+          centerTitle: true,
+          backgroundColor: Colors.amber,
+        ),
+        body: FutureBuilder<Map>(
+          future: getData(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                  child: Text(
+                    "Carregando Dados...",
+                    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                  ),
+                );
+                break;
+              default:
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text(
+                      "Erro ao carregar Dados...",
+                      style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                    ),
+                  );
+                } else {
+                  return Container(
+                    color: Colors.green,
+                  );
+                }
+            }
+          },
+        ));
   }
 }
